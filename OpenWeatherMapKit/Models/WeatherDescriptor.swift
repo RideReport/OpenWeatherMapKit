ccc5b3b26c16f8c827fa144915d0238625e5cd04
William Henderson <william.c.henderson@gmail.com>
2022-05-11 11:19:50 -0700

    map weather code to emoji

--- OpenWeatherMapKit/Models/WeatherDescriptor.swift
+++ OpenWeatherMapKit/Models/WeatherDescriptor.swift
@@ -13,4 +13,34 @@ public struct WeatherDescriptor : Codable {
     public var main: String
     public var description: String
     public var icon: String
+    
+    public var emoji: String {
+        switch self.id {
+            // Based on https://openweathermap.org/weather-conditions
+            
+            // Thunderstorm
+            case 200...202: return "⛈"
+            case 210...299: return "🌩"
+               
+            // Drizzle
+            case 300...399: return "🌧"
+               
+            // Rain
+            case 500...599: return "🌧"
+               
+            // Snow
+            case 600...699: return "🌨"
+               
+            // Atmosphere
+            case 701...799: return "🌫"
+                
+            // Clear
+            case 800: return "☀️"
+                
+            // Clouds
+            case 801...809: return "☁️"
+                   
+            default: return "☀️"
+           }
+       }
 }
